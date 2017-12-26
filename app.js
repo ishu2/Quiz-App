70809ba8f210ca9c618898767b943109bcc22171
ishu2 <rathiishita123@gmail.com>
2017-12-26 16:51:34 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+function populate(){
+    if(quiz.isEnded()){
+        showScores();
+    }
+    else{
+        // show question
+        var element=document.getElementById("question");
+        element.innerHTML=quiz.getQuestionIndex().text;
+
+        //show options
+        var choices=quiz.getQuestionIndex().choices;
+        for(var i=0;i<choices.length;i++){
+            var element=document.getElementById("choice"+i);
+            element.innerHTML=choices[i];
+            guess("btn"+i,choices[i]);
+        }
+        showProgress();
+    }
+}
+
+function guess(id,guess){
+    var button=document.getElementById(id);
+    button.onclick=function(){
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+function showProgress(){
+    var currentQuestionNumber=quiz.questionIndex+1;
+    var element=document.getElementById("progress");
+    element.innerHTML="Question "+currentQuestionNumber+" of "+quiz.questions.length;   
+}
+
+function showScores(){
+    var gameOverHTML="<h1>Result</h1>";
+    gameOverHTML+="<h2 id='score'>Your scores: "+quiz.score+"</h2>";
+    var element=document.getElementById("quiz");
+    element.innerHTML=gameOverHTML;
+}
+
+// Question bank
+var questions=[
+    new Question("Which one is not an object oriented programming language?",["Java","C#","C++","C"],"C"),
+    new Question("Which lenguage is used for styling web pages?",["HTML","jQuery","CSS","XML"],"CSS"),
+    new uestion("There are _______ main components of object oriented programming.",["1","6","2","4"],"4"),
+    new Question("Which language is used for web apps?",["PHP","Python","Javascript","All"],"All"),
+    new Question("MVC is a _____.",["Language","Library","Framework","All"],"Framework")
+]
+
+// create quiz
+var quiz=new Quiz(questions);
+
+//display quiz
+populate();
